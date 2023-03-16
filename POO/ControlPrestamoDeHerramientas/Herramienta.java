3ccba2063b25e4061c52b324e699ddb5a9c490b6
AlexLessus <118391189+AlexLessus@users.noreply.github.com>
2023-03-16 14:05:28 -0600

    Update Herramienta.java

--- POO/ControlPrestamoDeHerramientas/Herramienta.java
+++ POO/ControlPrestamoDeHerramientas/Herramienta.java
@@ -1,3 +1,35 @@
 public class Herramienta {
-
+   private int id;
+   private String nombre;
+   private String marca;
+   private char estado;
+   
+   public Herramienta(int id, String nombre, String marca, char estado) {
+      this.id = id;
+      this.nombre = nombre;
+      this.marca = marca;
+      this.estado = estado;
+   }
+   
+   public int getId() {
+      return id;
+   }
+   public String getNombre() {
+      return nombre;
+   }
+   public String getMarca() {
+      return marca;
+   }
+   public char getEstado() {
+      return estado;
+   }
+   
+   public String toString() {
+      return "Datos del trabajador\n" + 
+             "ID----: " + id + "\n" +
+             "Nombre: " + nombre + "\n" +
+             "Marca-: " + marca + "\n" +
+             "Estado: " + estado;    
+   }
+   
 }
