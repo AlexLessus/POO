1c12d3d7910a1374df96fa16fc0b4b008be6c719
AlexLessus <118391189+AlexLessus@users.noreply.github.com>
2023-03-16 20:45:33 -0600

    Update Prestamo.java

--- POO/ControlPrestamoDeHerramientas/Prestamo.java
+++ POO/ControlPrestamoDeHerramientas/Prestamo.java
@@ -1,3 +1,42 @@
 public class Prestamo {
-
+   private int numPrestamo;
+   private int idTrabajador;
+   private int idHerramienta;
+   private String fechaPrestamo;
+   private String fechaDevolucion;
+   private char estado;
+   
+   public Prestamo(int numPrestamo, int idTrabajador, int idHerramienta, String fechaPrestamo, char estado) {
+      this.numPrestamo = numPrestamo;
+      this.idTrabajador = idTrabajador;
+      this.idHerramienta = idHerramienta;
+      this.fechaPrestamo = fechaPrestamo;
+      this.estado = estado;
+   }
+   
+   public int getNumPrestamo() {
+      return numPrestamo;
+   }
+   public int getIdTrabajador() {
+      return idTrabajador;
+   }
+   public int getIdHerramienta() {
+      return idHerramienta;
+   }
+   public String getFechaPrestamo() {
+      return fechaPrestamo;
+   }
+   public String getFechDevolucion() {
+      return fechaDevolucion;
+   }
+   public char getEstado() {
+      return estado;
+   }
+   
+   public void setFechaDevolucion(String fechaDevolucion) {
+      this.fechaDevolucion = fechaDevolucion;
+   }
+   public void setEstado(char estado) {
+      this.estado = estado;
+   }
 }
