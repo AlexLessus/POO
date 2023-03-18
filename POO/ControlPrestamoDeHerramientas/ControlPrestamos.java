b7304187070e1aaa1f67a71854d0749fb1a94f5f
AlexLessus <118391189+AlexLessus@users.noreply.github.com>
2023-03-18 09:58:35 -0600

    Update ControlPrestamos.java

--- POO/ControlPrestamoDeHerramientas/ControlPrestamos.java
+++ POO/ControlPrestamoDeHerramientas/ControlPrestamos.java
@@ -1,7 +1,10 @@
 public class ControlPrestamos {
-   private Trabajador trabajadores[] = new Trabajador[110];
-   private Herramienta herramientas[] = new Herramienta[250];
-   private Prestamo prestamos[] = new Prestamo[500];
+   public final static int CANTIDAD_TRABAJADORES = 110;
+   public final static int CANTIDAD_HERRAMIENTAS = 250;
+   public final static int CANTIDAD_PRESTAMOS = 500;
+   private Trabajador trabajadores[] = new Trabajador[CANTIDAD_TRABAJADORES];
+   private Herramienta herramientas[] = new Herramienta[CANTIDAD_HERRAMIENTAS];
+   private Prestamo prestamos[] = new Prestamo[CANTIDAD_PRESTAMOS];
    private int contTrabajador; 
    private int contHerramienta;
    private int contPrestamo;
@@ -38,4 +41,27 @@ public class ControlPrestamos {
    }
    
    
+   public int busquedaTrabajador(int id) {
+      for(int i = 0; i < contTrabajador; i++) {
+         if(trabajadores[i].getId() == id) {
+            return i;
+         }
+      }
+      return -1;
+   }
+   public int busquedaHerramienta(int id) {
+      for(int i = 0; i < contHerramienta; i++) {
+         if(herramientas[i].getId() == id) {
+            return i;
+         }
+      }
+      return -1;
+   }
+   
+   public Trabajador getTrabajador(int celda) {
+      return trabajadores[celda];
+   }
+   public Herramienta getHerramienta(int celda) {
+      return herramientas[celda];
+   }
 }
